ac6416e1742a61b67d5e6e50a7749ec338c85d07
José Paulo Dias Chagas Coutinho <paulodcoutinho@hotmail.com>
2026-08-27 22:34:33 -0300

    Versão 2 do App Games da aula pratica

---
+++ Aula_pratica_2/js/script.js
@@ -0,0 +1,36 @@
+const API = 'https://www.gamerpower.com/api/giveaways?type=game';
+
+async function carregarJogos() {
+  const grid = document.querySelector('#games');
+
+  try {
+    const resposta = await fetch(API);
+
+    if (!resposta.ok) {
+      throw new Error('Erro ao acessar a API: ' + resposta.status);
+    }
+
+    const jogos = await resposta.json();
+
+    console.log(jogos);
+
+    grid.innerHTML = '';
+
+    jogos.forEach(jogo => {
+      grid.innerHTML += `
+                    <article class="game">
+                        <img src="${jogo.thumbnail}" alt="${jogo.title}">
+                        <h3>${jogo.title}</h3>
+                    </article>
+                `;
+    });
+  } catch (erro) {
+    console.error('Erro ao carregar os jogos:', erro);
+
+    grid.innerHTML = `
+                <p>Não foi possível carregar os jogos.</p>
+            `;
+  }
+}
+
+carregarJogos();
